36d5ee508a10cd7b7e0c13c00974908507f3f6cc
akilinjamam <akilinjamam@gmail.comm>
2022-11-08 08:04:36 +0600

    home-page-modified

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Home.css';
 import theImage from './images/job-task-home-image.png'
+import addIcon from './images/add-icon.png'
 
 const Home = () => {
     return (
         <div className='w-5/6 mx-auto'>
             <div className='flex justify-between div-1'>
                 <h1 className='font-bold text-2xl'>Media Library</h1>
-                <button className='bg'> <p>Upload New Image</p></button>
+                <div className='flex items-center bg'>
+                    <img className='icon' src={addIcon} alt="" />
+                    <button > <p>Upload New Image</p></button>
+                </div>
             </div>
 
             <div className='img-width'>
