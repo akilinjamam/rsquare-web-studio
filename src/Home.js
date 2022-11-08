2c61143d84b0db8fdb21a54750ce63c99385d7ca
akilinjamam <akilinjamam@gmail.comm>
2022-11-08 06:17:09 +0600

    home-page-done

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,17 @@ import theImage from './images/job-task-home-image.png'
 
 const Home = () => {
     return (
-        <div>
-            <div className='flex justify-between w-5/6 mx-auto mt-7'>
+        <div className='w-5/6 mx-auto'>
+            <div className='flex justify-between div-1'>
                 <h1 className='font-bold text-2xl'>Media Library</h1>
                 <button className='bg'> <p>Upload New Image</p></button>
             </div>
 
-            <div>
-                <img src={theImage} alt="" />
+            <div className='img-width'>
+                <img className='mx-auto' src={theImage} alt="" />
             </div>
+
+            <p className='last-font'>Click on 'Upload' to start adding images</p>
         </div>
     );
 };
